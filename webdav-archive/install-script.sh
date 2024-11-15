083cb8bf49ab41564c1417c4f224ecac5fc9a0fa
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-11-15 14:43:13 +0300

    edit install-script for webwav

--- webdav-archive/install-script.sh
+++ webdav-archive/install-script.sh
@@ -1,6 +1,15 @@
 sudo dnf install httpd
 
 sudo mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak
+# edit /etc/httpd/conf/httpd.conf 
+
+sudo dnf install mod_ssl
+# edit /etc/httpd/conf.d/ssl.conf
+
+sudo cp install-dumbproxy/certs/rootCA.pem /etc/pki/ca-trust/source/anchors/ca-proxy-wd-202411.crt
+sudo update-ca-trust extract
+sudo cp install-dumbproxy/certs/1.2.3.4.pem /etc/pki/tls/certs/
+sudo cp install-dumbproxy/certs/1.2.3.4-key.pem /etc/pki/tls/private/
 
 sudo mkdir /srv/webdavlock
 sudo mkdir /srv/webdav1
@@ -11,17 +20,8 @@ sudo chown apache:apache /srv/webdav2
 
 sudo cp archive-webdav/etc-httpd-conf/dav_passwords /etc/httpd/conf/
 sudo cp archive-webdav/etc-httpd-conf.d/webdav.conf /etc/httpd/conf.d/
-#sudo cp archive-webdav/etc-httpd-conf.d/ssl.conf /etc/httpd/conf.d/
-
-sudo dnf install mod_ssl
-
-sudo cp install-dumbproxy/certs/rootCA.pem /etc/pki/ca-trust/source/anchors/ca-proxy-wd-202411.crt
-sudo update-ca-trust extract
-sudo cp install-dumbproxy/certs/1.2.3.4.pem /etc/pki/tls/certs/
-sudo cp install-dumbproxy/certs/1.2.3.4-key.pem /etc/pki/tls/private/
 
 systemctl enable --now httpd 
-
 
 sudo dnf install policycoreutils-python-utils
 sudo semanage port -l | grep http
