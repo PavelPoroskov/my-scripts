b2cf355b59ae95443b7d804a890768ecd2f1c6b4
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-12-11 06:31:47 +0300

    move dir browser-policies to repo my-web-extensions

--- browser-policies/set-all-browser-policies.sh
+++
@@ -1,8 +0,0 @@
-sudo mkdir -p /etc/firefox/policies
-sudo cp policies.json /etc/firefox/policies/
-
-sudo mkdir -p /etc/opt/chrome/policies/managed
-sudo cp my-chrome-policy.json /etc/opt/chrome/policies/managed/
-
-sudo mkdir -p /etc/chromium/policies/managed
-sudo cp my-chrome-policy.json /etc/chromium/policies/managed/
